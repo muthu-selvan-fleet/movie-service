a7eae0bd53ff5e639d26ca63b5659237899e943c
Muthu Selvan <muthu_selvan@capturisk.com>
2022-04-19 11:15:57 +0530

    Adding upvote and downvote count

--- src/main/java/com/fs/movie/service/controller/MoviesController.java
+++ src/main/java/com/fs/movie/service/controller/MoviesController.java
@@ -18,13 +18,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fs.movie.service.model.Genre;
 import com.fs.movie.service.model.Movie;
@@ -92,6 +86,7 @@ public class MoviesController {
 		return new ResponseEntity<>( HttpStatus.NOT_FOUND);
 	}
 
+
 	@PostMapping("/movie")
 	public ResponseEntity<Movie> addMovie(@RequestBody final MovieModel movieModel) {
 		
@@ -100,6 +95,8 @@ public class MoviesController {
 		
 		movie.setId(movieModel.getId());
 		movie.setName(movieModel.getName());
+		movie.setUpVoteCount(movieModel.getUpVoteCount());
+		movie.setDownVoteCount(movieModel.getDownVoteCount());
 		
 		movieModel.getGenre().forEach(eachGenre -> {
 			var genre = new Genre();
